297251ad412c2b966c768ab4f0a39241c7707152
jgpark-1 <jgpark7281@gmail.com>
2025-01-03 00:01:26 +0900

    style(apps/web): lint fix

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,10 +1,10 @@
-import { Button } from "@repo/ui/components/ui/button";
+import { Button } from "@repo/ui/components/ui/button"
 
 export default function Page() {
   return (
-    <div className="relative flex flex-col min-h-screen bg-background">
-      <main className="flex-1 flex flex-col items-center justify-center">
-        <h1 className="text-xl font-bold mb-2">
+    <div className="relative flex min-h-screen flex-col bg-background">
+      <main className="flex flex-1 flex-col items-center justify-center">
+        <h1 className="mb-2 text-xl font-bold">
           Turborepo & Shadcn/ui
         </h1>
         <Button>
@@ -12,5 +12,5 @@ export default function Page() {
         </Button>
       </main>
     </div>
-  );
+  )
 }
