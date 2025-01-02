b2bead5c3de085409d947d5f694c67a78c252df8
jgpark-1 <jgpark7281@gmail.com>
2025-01-03 00:00:58 +0900

    feat(apps/web): add tailwindcss h-18 class

--- packages/ui/tailwind.config.ts
+++ packages/ui/tailwind.config.ts
@@ -79,6 +79,9 @@ const config = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+      height: {
+        "18": "4.5rem",
+      }
     },
   },
   plugins: [tailwindcssAnimate],
