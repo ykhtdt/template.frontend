4e0af94c1db025de893e8dd86d828b641ad5e7bc
Jonggwang Park <jgpark7281@gmail.com>
2025-05-08 19:11:14 +0900

    feat: 테스트 코드에 의한 HomePage 수정

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,12 +1,9 @@
-import { Button } from "@workspace/ui/components/button"
+import { HomePage } from "@/pages/home"
 
-export default function Page() {
+const Page = () => {
   return (
-    <div className="flex items-center justify-center min-h-svh">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello World</h1>
-        <Button size="sm">Button</Button>
-      </div>
-    </div>
+    <HomePage />
   )
 }
+
+export default Page
